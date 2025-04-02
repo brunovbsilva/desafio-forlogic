144f911844ca06b758d6be8a86d3f237f84ffa01
Bruno Silva <brunovinicius0704@gmail.com>
2025-04-02 10:16:43 -0300

    feat: add router link to layout

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { ButtonComponent } from '@shared/components/button/button.component';
 
 @Component({
 	selector: 'app-layout',
-	imports: [RouterOutlet, ButtonComponent],
+	imports: [RouterOutlet, ButtonComponent, RouterLink],
 	templateUrl: './layout.component.html',
 	styleUrl: './layout.component.scss',
 })
