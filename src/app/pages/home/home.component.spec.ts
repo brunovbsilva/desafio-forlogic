7ec4fe155e31e83d124afce6b7c98a0cfed3b36c
Bruno Silva <brunovinicius0704@gmail.com>
2025-04-02 10:20:39 -0300

    feat: home and cards

--- src/app/pages/home/home.component.spec.ts
+++ src/app/pages/home/home.component.spec.ts
@@ -3,21 +3,34 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HomeComponent } from './home.component';
 
 describe('HomeComponent', () => {
-  let component: HomeComponent;
-  let fixture: ComponentFixture<HomeComponent>;
+	let component: HomeComponent;
+	let fixture: ComponentFixture<HomeComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [HomeComponent]
-    })
-    .compileComponents();
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			imports: [HomeComponent],
+		}).compileComponents();
 
-    fixture = TestBed.createComponent(HomeComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+		fixture = TestBed.createComponent(HomeComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
+
+	describe('should contains card-rows', () => {
+		const cardRow = '[data-test="card-row"]';
+		const totalCard = '[data-test="total-card"]';
+		const lastMonthCard = '[data-test="last-month-card"]';
+		const pendingCard = '[data-test="pending-card"]';
+		it('card row exists', () => expect(fixture.debugElement.nativeElement.querySelector(cardRow)).toBeTruthy());
+		it('total card', () => expect(fixture.debugElement.nativeElement.querySelector(totalCard)).toBeTruthy());
+		it('last month card', () => expect(fixture.debugElement.nativeElement.querySelector(lastMonthCard)).toBeTruthy());
+		it('pending card', () => expect(fixture.debugElement.nativeElement.querySelector(pendingCard)).toBeTruthy());
+	});
+
+  const mainContent = '[data-test="main-content"]';
+  it('should contains main content', () => expect(fixture.debugElement.nativeElement.querySelector(mainContent)).toBeTruthy());
 });
---
+++ src/app/shared/components/card/card.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  imports: [],
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.scss',
+  host: {
+    '[class.card]': 'true'
+  }
+})
+export class CardComponent {
+
+}
