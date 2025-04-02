e83c442f01441c7a66be0d72e39b41019fc35bff
Bruno Silva <brunovinicius0704@gmail.com>
2025-04-02 10:44:05 -0300

    feat: user-list

--- src/app/pages/report-pages/report-user-list/report-user-list.component.ts
+++ src/app/pages/report-pages/report-user-list/report-user-list.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { CardComponent } from '../../../shared/components/card/card.component';
+import { ButtonComponent } from '@shared/components/button/button.component';
 
 @Component({
-  selector: 'app-report-user-list',
-  imports: [],
-  templateUrl: './report-user-list.component.html',
-  styleUrl: './report-user-list.component.scss'
+	selector: 'app-report-user-list',
+	imports: [CardComponent, ButtonComponent],
+	templateUrl: './report-user-list.component.html',
+	styleUrl: './report-user-list.component.scss',
 })
-export class ReportUserListComponent {
-
-}
+export class ReportUserListComponent {}
